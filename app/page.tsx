90b87c290f3f50b33e58fdb5438123cd2c20c76b
Chris Banhanna <c@Christophers-MacBook-Air.local>
2026-03-29 14:40:50 +0100

    Force dynamic fetch for Supabase locations

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import ClientHomeSupabase from "./components/ClientHomeSupabase";
 import { getLocations } from "./lib/queries";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   const locations = await getLocations();
 
